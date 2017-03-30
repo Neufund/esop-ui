fa713f2fa0382cb043bff78ad46ba971212a302a
Tomek Banach <tomek@banciur.org>
2017-03-30 15:14:28 +0200

    Implement part of esop view

--- src/app/Esop.js
+++ src/app/Esop.js
@@ -1,8 +1,87 @@
 import React from 'react';
 import './Esop.scss';
 
+import chart from '../images/esop_chart.jpg';
+
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+
 export default () => {
     return (
-        <div>esop</div>
+        <div className="esop">
+            <div className="row">
+                <div className="col-xs-12 col-md-3">
+                    <h1>Neufund ESOP details</h1>
+                </div>
+                <div className="col-xs-12 col-md-7 read_more">
+                    <RaisedButton className="read_more_button" label="Read more what ESOP is"/>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-xs-12">
+                        <h2>Contract addresses:</h2>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-xs-12">
+                    <Table selectable={false}>
+                        <TableBody displayRowCheckbox={false}>
+                            <TableRow >
+                                <TableRowColumn>Root of trust</TableRowColumn>
+                                <TableRowColumn>0x0046adE103035E8d9B1E8143Ec077F7cfcB47c2f</TableRowColumn>
+                            </TableRow>
+                            <TableRow>
+                                <TableRowColumn>ESOP contract</TableRowColumn>
+                                <TableRowColumn>0x0046adE103035E8d9B1E8143Ec077F7cfcB47c2f</TableRowColumn>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-xs-12">
+                    <h2>Contract parameters:</h2>
+                </div>
+            </div>
+
+            <div className="row">
+                <Tabs>
+                    <Tab label="Parameters" >
+                        <div className="col-xs-12 contract_parameters">
+                            <TextField floatingLabelText="cliff duration" className="contract_parameter" value="1 year" disabled={true}/>
+                            <TextField floatingLabelText="vesting duration" className="contract_parameter" value="4 years" disabled={true}/>
+                            <TextField floatingLabelText="max fadeout promille" className="contract_parameter" value="20%" disabled={true}/>
+                            <TextField floatingLabelText="exit bonus promille" className="contract_parameter" value="25%" disabled={true}/>
+                            <TextField floatingLabelText="new employee pool promille" className="contract_parameter" value="10%" disabled={true}/>
+                            <TextField floatingLabelText="total options" className="contract_parameter" value="1 000 000" disabled={true}/>
+                        </div>
+                    </Tab>
+                    <Tab label="Visualisation" >
+                        <div className="col-xs-12">
+                            <img className="contract_chart" src={chart} />
+                        </div>
+                    </Tab>
+                </Tabs>
+            </div>
+
+            <div className="row">
+                <div className="col-xs-12">
+                    <h2>Contract stats:</h2>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-xs-12">
+                    <h2>Employees:</h2>
+                </div>
+            </div>
+
+        </div>
     )
 };
--- src/muiTheme.js
+++ src/muiTheme.js
@@ -1,7 +1,9 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// color are taken from _colors.scss files
 export default getMuiTheme({
     palette: {
-        primary1Color: "#d5e20f",
+        primary1Color: "#686e75", //this one is just taken randomly
         textColor: "#424a52",
+        accent1Color: "#d5e20f"
     }
 });
