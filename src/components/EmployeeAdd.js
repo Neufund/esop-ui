9f5d6914f8623c6ba6c80d2fbe1afb8f532515eb
Tomek Banach <tomek@banciur.org>
2017-04-07 16:46:19 +0200

    Align fields of adding employee component

--- src/components/EmployeeAdd.js
+++ src/components/EmployeeAdd.js
@@ -11,11 +11,11 @@ export default (props) => {
         <div className="row">
             <div className="col-xs-12 employee_add">
                 <h3>Add employee:</h3>
-                <TextField floatingLabelText="public key" className="employee_parameter"/>
-                <TextField floatingLabelText="started working" className="employee_parameter"/>
-                <TextField floatingLabelText="time to sign [days]" className="employee_parameter"/>
+                <TextField floatingLabelText="public key" className="employee_parameter"/> <br />
+                <TextField floatingLabelText="started working" className="employee_parameter"/> <br />
+                <TextField floatingLabelText="time to sign [days]" className="employee_parameter"/> <br />
                 <Checkbox label="Use bonus shares"/>
-                <TextField floatingLabelText="extra options" className="employee_parameter"/>
+                <TextField floatingLabelText="extra options" className="employee_parameter"/> <br />
                 <RaisedButton label="Add user"/>
             </div>
         </div>
