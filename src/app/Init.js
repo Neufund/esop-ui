982dcdd79c5f2190b13d32a64b29a2a030577f13
Tomek Banach <tomek@banciur.org>
2017-04-12 14:29:34 +0200

    Init component have fields that corresponds to openESOP function

--- src/app/Init.js
+++ src/app/Init.js
@@ -4,57 +4,72 @@ import './Init.scss';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-export default () => {
-    return (
-        <div className="new_esop">
-            <div className="row">
-                <div className="col-xs-12">
-                    <h1>New ESOP</h1>
-                </div>
-            </div>
+export default class Init extends React.Component {
 
-            <div className="row">
-                <div className="col-xs-12">
-                    <h2>ESOP contract address</h2>
-                    <TextField floatingLabelText="ESOP contract address" style={{width: "32.000rem"}}/>
-                </div>
-            </div>
+    constructor(props) {
+        super(props);
+        this.store = props.store;
+    }
 
-            <div className="row">
-                <div className="col-xs-12">
-                    <h2>ESOP contract parameters</h2>
-                </div>
-            </div>
+    componentDidMount() {
+        this.unsubscribe = this.store.subscribe(() => this.forceUpdate());
+    }
 
-            <div className="row">
-                <div className="col-xs-12 contract_parameters">
-                    <TextField floatingLabelText="cliff duration" className="contract_parameter"/>
-                    <TextField floatingLabelText="vesting duration" className="contract_parameter"/>
-                    <TextField floatingLabelText="max fadeout promille" className="contract_parameter"/>
-                    <TextField floatingLabelText="exit bonus promille" className="contract_parameter"/>
-                    <TextField floatingLabelText="new employee pool promille" className="contract_parameter"/>
-                    <TextField floatingLabelText="total options" className="contract_parameter"/>
-                </div>
-            </div>
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
 
-            <div className="row">
-                <div className="col-xs-12">
-                    <h2>ESOP paper agreement</h2>
-                </div>
-            </div>
 
-            <div className="row">
-                <div className="col-xs-12 contract_data">
-                    <TextField floatingLabelText="IPFS hash" style={{width: "32.000rem"}}/>
-                </div>
-            </div>
+    render() {
 
-            <div className="row">
-                <div className="col-xs-12 start">
-                    <RaisedButton label="Start ESOP!" onClick={() => alert("This will start ESOP you will see nice spinner and after some time will be redirected to public view of contract")} />
-                </div>
-            </div>
+        let ESOPState = this.store.getState().ESOP;
 
-        </div>
-    )
+        return (
+            <div className="new_esop">
+                <div className="row">
+                    <div className="col-xs-12">
+                        <h1>New ESOP</h1>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12">
+                        <h2>ESOP contract addresses</h2>
+                        <TextField floatingLabelText="RoT contract address" value={ESOPState.RoTAddress}
+                                   style={{width: "32.000rem"}}
+                                   disabled={true}/> <br />
+                        <TextField floatingLabelText="ESOP contract address" value={ESOPState.ESOPAddress}
+                                   style={{width: "32.000rem"}}
+                                   disabled={true}/> <br />
+                        <TextField floatingLabelText="OptionsCalculator contract address"
+                                   value={ESOPState.OptionsCalculatorAddress}
+                                   style={{width: "32.000rem"}} disabled={true}/> <br />
+                        <TextField floatingLabelText="EmployeesList contract address" value={ESOPState.EmployeesList}
+                                   style={{width: "32.000rem"}} disabled={true}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12">
+                        <h2>ESOP contract parameters</h2>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12">
+                        <TextField floatingLabelText="total pool options" className="contract_parameter"/> <br />
+                        <TextField floatingLabelText="ESOP Legal Wrapper IPFS Hash" style={{width: "32.000rem"}}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12 start">
+                        <RaisedButton label="Start ESOP!"
+                                      onClick={() => alert("This will start ESOP you will see nice spinner and after some time will be redirected to public view of contract")}/>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
 };
