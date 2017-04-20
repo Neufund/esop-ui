73e114ac9f79fc852eba3306614a68ec1af52672
Tomek Banach <tomek@banciur.org>
2017-04-20 23:28:27 +0200

    Remove unused Employee view

--- src/app/Employee.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Employee.scss';
-
-export default (props) => {
-    console.log(props);
-    return (
-        <div>hi employee {props.params.publicKey != "undefined" && props.params.publicKey}</div>
-    )
-};
