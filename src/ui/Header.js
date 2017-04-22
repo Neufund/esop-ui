71fd5de8b18720aad02e07523163ababa55e55a0
Tomek Banach <tomek@banciur.org>
2017-04-22 16:30:26 +0200

    Fix header messages about logged user

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -3,35 +3,20 @@ import './Header.scss';
 
 export default ({userPK, userType}) => {
     return (
-        <div>
-            <div className="row">
-                <div className="col-xs-12 col-md-10 col-md-offset-1">
-                    <div className="header">
-                        ESOP Dapp
-                    </div>
+        <div className="row">
+            <div className="col-xs-12 col-md-10 col-md-offset-1">
+                <div className="header">
+                    ESOP Dapp
                 </div>
+                {userPK != undefined ?
+                    <p>Hello user: {userPK}</p>
+                    :
+                    <p>Please enable your metamask or plug nano ledger so we can identify you!</p>
+                }
+                {userType != "anonymous" &&
+                <p>Your position: {userType}</p>
+                }
             </div>
-            {userPK != undefined &&
-            <div className="row">
-                <div className="col-xs-12 col-md-10 col-md-offset-1">
-                    Hello user: {userPK}
-                </div>
-            </div>
-            }
-
-            {userType != "anonymous" ?
-                <div className="row">
-                    <div className="col-xs-12 col-md-10 col-md-offset-1">
-                        Your position: {userType}
-                    </div>
-                </div>
-                :
-                <div className="row">
-                    <div className="col-xs-12 col-md-10 col-md-offset-1">
-                        <p>Please enable your metamask or plug nano ledger so we can identify you!</p>
-                    </div>
-                </div>
-            }
         </div>
     )
 }
