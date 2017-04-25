dd04f25874f398d999cef2c3b74dc73a04f7aa4e
Tomek Banach <tomek@banciur.org>
2017-04-25 09:52:39 +0200

    Change how table is created and format numbers in ContractStatus

--- src/components/ContractStatus.js
+++ src/components/ContractStatus.js
@@ -3,32 +3,42 @@ import {Table, TableBody, TableRow, TableRowColumn} from 'material-ui/Table';
 import ContractUtils from '../ContractUtils'
 
 export default ({contractState}) => {
+    let numberFormatter = new Intl.NumberFormat();
+    let table = [
+        {
+            desc: "ESOP state",
+            value: ContractUtils.getESOPStateName(contractState.esopState)
+        },
+        {
+            desc: "Employees in ESOP",
+            value: contractState.employees.length
+        },
+        {
+            desc: "Total pool options",
+            value: numberFormatter.format(contractState.totalPoolOptions)
+        },
+        {
+            desc: "Remaning pool options",
+            value: numberFormatter.format(contractState.remainingPoolOptions)
+        },
+        {
+            desc: "Extra options issued",
+            value: numberFormatter.format(contractState.totalExtraOptions)
+        }
+    ];
+
     return (
         <div className="row">
             <div className="col-xs-12">
                 <h2 className="contracts_stats">Contract status:</h2>
                 <Table selectable={false}>
                     <TableBody displayRowCheckbox={false}>
-                        <TableRow >
-                            <TableRowColumn>ESOP state</TableRowColumn>
-                            <TableRowColumn>{ContractUtils.getESOPStateName(contractState.esopState)}</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>Employees in esop</TableRowColumn>
-                            <TableRowColumn>{contractState.employees.length}</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>Total pool options</TableRowColumn>
-                            <TableRowColumn>{contractState.totalPoolOptions}</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>Remaning pool options</TableRowColumn>
-                            <TableRowColumn>{contractState.remainingPoolOptions}</TableRowColumn>
-                        </TableRow>
-                        <TableRow>
-                            <TableRowColumn>Extra options issued</TableRowColumn>
-                            <TableRowColumn>{contractState.totalExtraOptions}</TableRowColumn>
-                        </TableRow>
+                        {table.map((row, index) =>
+                            <TableRow key={index}>
+                                <TableRowColumn>{row.desc}</TableRowColumn>
+                                <TableRowColumn>{row.value}</TableRowColumn>
+                            </TableRow>
+                        )}
                     </TableBody>
                 </Table>
             </div>
