29c5b5eae38bab06c96e30cff1a3475923f40b7b
Tomek Banach <tomek@banciur.org>
2017-04-25 09:47:34 +0200

    Change how table is created and format numbers in EmployeeSignESOP

--- src/components/EmployeeSignESOP.js
+++ src/components/EmployeeSignESOP.js
@@ -9,82 +9,82 @@ import moment from 'moment'
 
 export default ({employee, ESOPState, signHandler}) => {
     let dateFormat = 'YY-MM-DD'; //TODO: this should go to configuration
+    let numberFormatter = new Intl.NumberFormat();
+
+    let table = [
+        {
+            desc: "Public Key (Ethereum Address of Employee)",
+            value: <div>{employee.address} <a key={1} className="inline_link" target="_blank"
+                                              href={`https://etherscan.io/address/${employee.address}`}>
+                <FontIcon className="material-icons material_icon_table">link</FontIcon></a></div>
+        },
+        {
+            desc: "Issued Options",
+            value: numberFormatter.format(employee.poolOptions + employee.extraOptions)
+        },
+        {
+            desc: "Pool Options",
+            value: numberFormatter.format(employee.poolOptions)
+        },
+        {
+            desc: "Extra Options",
+            value: numberFormatter.format(employee.extraOptions)
+        },
+        {
+            desc: "Strike price",
+            value: `EUR ${ESOPState.strikePrice} per share`
+        },
+        {
+            desc: "Issue Date",
+            value: moment.unix(employee.issueDate).format(dateFormat)
+        },
+        {
+            desc: "Vesting Period",
+            value: humanReadableDuration(ESOPState.vestingPeriod)
+        },
+        {
+            desc: "Cliff period",
+            value: humanReadableDuration(ESOPState.cliffPeriod)
+        },
+        {
+            desc: "Bonus Options",
+            value: ESOPState.bonusOptionsPromille
+        },
+        {
+            desc: "Fade Out Period",
+            value: <b>soon</b>
+        },
+        {
+            desc: "Residual amount",
+            value: <b>soon</b>
+        },
+        {
+            desc: "Time to accept Option-Offer",
+            value: moment.unix(employee.timeToSign).format(dateFormat)
+        },
+        {
+            desc: "EMPLOYEE STATUS",
+            value: ContractUtils.getEmployeeStateName(employee.state)
+        }
+    ];
+
     return (
         <div className="employee_sign_esop">
             <div className="row">
                 <div className="col-xs-12">
                     <h3>Fifth force GmBH identifed by address {ESOPState.companyAddress}
-                    <a className="inline_link" target="_blank" href={`https://etherscan.io/address/${ESOPState.companyAddress}`}>
-                        <FontIcon className="material-icons">link</FontIcon></a></h3>
+                        <a className="inline_link" target="_blank"
+                           href={`https://etherscan.io/address/${ESOPState.companyAddress}`}>
+                            <FontIcon className="material-icons">link</FontIcon></a></h3>
                     <h4>ESOP Subscription Form</h4>
                     <Table selectable={false}>
                         <TableBody displayRowCheckbox={false}>
-                            <TableRow>
-                                <TableRowColumn>Public Key (Ethereum Address of Employee</TableRowColumn>
-                                <TableRowColumn>{employee.address}
-                                    <a className="inline_link" target="_blank" href={`https://etherscan.io/address/${employee.address}`}>
-                                    <FontIcon className="material-icons material_icon_table">link</FontIcon></a></TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Issued Options</TableRowColumn>
-                                <TableRowColumn>{employee.poolOptions + employee.extraOptions}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Pool Options</TableRowColumn>
-                                <TableRowColumn>{employee.poolOptions}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Extra Options</TableRowColumn>
-                                <TableRowColumn>{employee.extraOptions}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Strike price</TableRowColumn>
-                                <TableRowColumn>EUR {ESOPState.strikePrice} per share</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Issue Date</TableRowColumn>
-                                <TableRowColumn>{moment.unix(employee.issueDate).format(dateFormat)}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Vesting Period</TableRowColumn>
-                                <TableRowColumn>{humanReadableDuration(ESOPState.vestingPeriod)}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Cliff period</TableRowColumn>
-                                <TableRowColumn>{humanReadableDuration(ESOPState.cliffPeriod)}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Bonus Options</TableRowColumn>
-                                <TableRowColumn>{ESOPState.bonusOptionsPromille}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Fade Out Period</TableRowColumn>
-                                <TableRowColumn><b>soon</b></TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Residual amount</TableRowColumn>
-                                <TableRowColumn><b>soon</b></TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>Time to accept Option-Offer</TableRowColumn>
-                                <TableRowColumn>{moment.unix(employee.timeToSign).format(dateFormat)}</TableRowColumn>
-                            </TableRow>
-
-                            <TableRow>
-                                <TableRowColumn>EMPLOYEE STATUS</TableRowColumn>
-                                <TableRowColumn>{ContractUtils.getEmployeeStateName(employee.state)}</TableRowColumn>
-                            </TableRow>
+                            {table.map((row, index) =>
+                                <TableRow key={index}>
+                                    <TableRowColumn>{row.desc}</TableRowColumn>
+                                    <TableRowColumn>{row.value}</TableRowColumn>
+                                </TableRow>
+                            )}
                         </TableBody>
                     </Table>
                 </div>
