ed29326084402c0890c754ed3d62a19b2c2085ff
Tomek Banach <tomek@banciur.org>
2017-04-25 17:02:44 +0200

    Opening ESOP form accepts real numbers

--- src/app/OpenESOP.js
+++ src/app/OpenESOP.js
@@ -56,10 +56,11 @@ export default class Init extends React.Component {
     };
 
     validateCliffPeriod = (value) => {
+        value = value.replace(',', '.');
         let validationOutcome = value === '' ? "please fill this field" : "";
 
         if (validationOutcome == "") {
-            let num = parseInt(value);
+            let num = Number.parseFloat(value);
             if (isNaN(num))
                 validationOutcome = 'value us not a number';
             else if (num <= 0)
@@ -70,10 +71,11 @@ export default class Init extends React.Component {
     };
 
     validateVestingPeriod = (value) => {
+        value = value.replace(',', '.');
         let validationOutcome = value === '' ? "please fill this field" : "";
 
         if (validationOutcome == "") {
-            let num = parseInt(value);
+            let num = Number.parseFloat(value);
             if (isNaN(num))
                 validationOutcome = 'value us not a number';
             else if (num <= 0)
@@ -208,13 +210,12 @@ export default class Init extends React.Component {
         if (!this.validateFields()) {
             return;
         }
-
         let year = 365 * 24 * 60 * 60;
 
         let totalPoolOptions = parseInt(this.state.totalPoolOptions);
         let ESOPLegalWrapperIPFSHash = web3.toBigNumber('0x' + new Buffer(this.state.ESOPLegalWrapperIPFSHash, 'ascii').toString('hex'));
-        let cliffPeriod = parseInt(this.state.cliffPeriod) * year;
-        let vestingPeriod = parseInt(this.state.vestingPeriod) * year;
+        let cliffPeriod = Math.floor(Number.parseFloat(this.state.cliffPeriod.replace(',', '.')) * year);
+        let vestingPeriod = Math.floor(Number.parseFloat(this.state.vestingPeriod.replace(',', '.')) * year);
         let residualAmount = parseInt(this.state.residualAmount) * 100;
         let bonusOptions = parseInt(this.state.bonusOptions) * 100;
         let newEmployeePool = parseInt(this.state.newEmployeePool) * 100;
