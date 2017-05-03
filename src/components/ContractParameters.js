914e502b8d9801bb62015743616e74260f8804e2
Tomek Banach <tomek@banciur.org>
2017-05-03 21:09:25 +0200

    Add tooltips to ContractParameters.js

--- src/components/ContractParameters.js
+++ src/components/ContractParameters.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './ContractParameters.scss';
 import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton'
+import IconButton from 'material-ui/IconButton';
 import {epochAsYears} from '../utils'
 
 export default ({contractParameters, isCEO, onClickTap}) => {
     let numberFormatter = new Intl.NumberFormat();
+
+    let tooltipStyles = {
+        fontSize: "1rem",
+        padding: "0.5rem"
+    };
+
     return (
         <div>
             <div className="row">
@@ -15,18 +21,30 @@ export default ({contractParameters, isCEO, onClickTap}) => {
             </div>
             <div className="row">
                 <div className="col-xs-12 contract_parameters">
+                    <IconButton iconClassName="material-icons" tooltip="total amount of shares assigned to employees"
+                                tooltipStyles={tooltipStyles}>info</IconButton>
                     <TextField floatingLabelText="total pool options" className="contract_parameter"
                                value={numberFormatter.format(contractParameters.totalPoolOptions)} disabled={true}/>
+
+                    <IconButton iconClassName="material-icons" tooltip="every employee gets a percentage of the remaining pool"
+                                tooltipStyles={tooltipStyles}>info</IconButton>
                     <TextField floatingLabelText="new employee pool" className="contract_parameter"
                                value={`${contractParameters.newEmployeePoolPromille / 100}%`} disabled={true}/>
+
                     <TextField floatingLabelText="vesting period" className="contract_parameter"
                                value={epochAsYears(contractParameters.vestingPeriod)} disabled={true}/>
+
+                    <IconButton iconClassName="material-icons" tooltip="period during which employee does not get any options "
+                                tooltipStyles={tooltipStyles}>info</IconButton>
                     <TextField floatingLabelText="cliff period" className="contract_parameter"
                                value={epochAsYears(contractParameters.cliffPeriod)} disabled={true}/>
+
                     <TextField floatingLabelText="residual amount" className="contract_parameter"
                                value={`${contractParameters.residualAmountPromille / 100}%`} disabled={true}/>
+
                     <TextField floatingLabelText="bonus options" className="contract_parameter"
                                value={`${contractParameters.bonusOptionsPromille / 100}%`} disabled={true}/>
+
                     <TextField floatingLabelText="strike price" className="contract_parameter"
                                value={`${contractParameters.STRIKE_PRICE} EUR`} disabled={true}/>
                 </div>
