1a5afab5c013b8967c52d1fafb9bdab28fc6ae25
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-05-03 22:10:40 +0200

    added texting and tooltips to open ESOP component

--- src/app/OpenESOP.js
+++ src/app/OpenESOP.js
@@ -5,10 +5,12 @@ import {web3} from '../web3'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton'
 
 import {validateDoc}from '../utils'
 import IPFSDialog from '../components/IPFSDialog';
+import Texting from '../texting'
 
 
 export default class Init extends React.Component {
@@ -395,116 +397,49 @@ export default class Init extends React.Component {
 
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.totalPoolOptionsProps}/>
-                                    <a href="#totalPool">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.totalPoolOptions} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.cliffPeriodProps}/>
-                                    <a href="#cliffPeriod">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.cliffPeriod} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.vestingPeriodProps}/>
-                                    <a href="#vestingPeriod">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.vestingPeriod} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.residualAmountProps}/>
-                                    <a href="#residualAmount">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.residualAmount} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.bonusOptionsProps}/>
-                                    <a href="#bonusOptions">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.bonusOptions} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.newEmployeePoolProps}/>
-                                    <a href="#newEmployeePool">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.newEmployeePool} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <div className="paramter_wrapper">
                                     <TextField {...textFieldsProps.optionsPerShareProps}/>
-                                    <a href="#optionsPerShare">
-                                        <FontIcon className="material-icons">info</FontIcon>
-                                    </a>
+                                    <IconButton tooltip={Texting.definitions.optionsPerShare} iconClassName="material-icons">info</IconButton>
                                 </div>
                                 <br />
                                 <TextField {...textFieldsProps.ESOPLegalWrapperIPFSHashProps}/>
-                                <a href="#ESOPLegalWrapperIPFSHash">
-                                    <FontIcon className="material-icons">info</FontIcon>
-                                </a>
-
+                                <IconButton tooltip={Texting.definitions.termsDocIPFSHash} iconClassName="material-icons">info</IconButton>
                                 <RaisedButton label="Validate doc" className="validate_button"
                                               onTouchTap={this.handleValidateDoc} />
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-xs-12 col-md-10 col-md-offset-1">
+                                <br/>
+                                <a href="https://github.com/Neufund/ESOP#esop-algorithm" target="_new">Read more on configuring ESOP Terms & Conditions</a>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-12 col-md-10 col-md-offset-1">
                                 <RaisedButton label="Initialize ESOP" onClick={this.handleOpenEsopButton}
                                               className="start_button"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="totalPool" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    total pool -
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="cliffPeriod" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    cliff period -
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="vestingPeriod" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    vesting period -
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="residualAmount" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    residual amount -
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="bonusOptions" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    bonus options -
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="newEmployeePool" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    new employee pool -
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="optionsPerShare" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    options per share
-                                </p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div id="ESOPLegalWrapperIPFSHash" className="col-xs-12 col-md-10 col-md-offset-1">
-                                <p>
-                                    ESOP Legal Wrapper IPFS Hash -
-                                </p>
                             </div>
                         </div>
                     </div>
---
+++ src/texting.js
@@ -0,0 +1,19 @@
+export default {
+    termsDocumentTitle: 'ESOP Terms & Conditions Document',
+    definitions: {
+        issuedOptions: 'A total number of options granted to Employee (pool options + extra options)',
+        poolOptions: 'Number of options granted to Employee as a certain percentage of remaining Pool Options at the time options-offer was made',
+        extraOptions: 'Number of options granted to Employee by Company outside of Pool Options',
+        strikePrice: 'A price of one ordinary share for which Employee has a right to exercise options',
+        optionsPerShare: 'Number of options corresponding to one ordinary share in Company',
+        issueDate: 'A date at which vesting starts',
+        vestingPeriod: 'A period of time after Issue Date during which number of options (vested options) increase linearly up until Issued Options amount',
+        cliffPeriod: 'Initial period of time of Vesting Period during which number of vested options is zero',
+        bonusOptions: 'A number of options that shall be granted to Employee on conversion event if Employee is still working at Company. Additional conditions may apply.',
+        fadeOutPeriod: 'A period of time after Work Termination during which number of vested options decreases linearly down until Residual Amount',
+        residualAmount: 'A percentage of Issued Options that remains with Employee at the end of Fade Out Period',
+        totalPoolOptions: 'Total number of Pool Options to be distributed to Employees',
+        termsDocIPFSHash: 'IPFS hash of a ESOP Terms & Conditions Document that correspond to ESOP smart contract',
+        newEmployeePool: 'A percentage of remaining Pool Options that will be offered to new employee'
+    }
+}
