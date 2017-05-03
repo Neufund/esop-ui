5acb9227e38468d43b23a8c9f73137b2f27c31eb
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-05-03 23:38:23 +0200

    ipfs_tags aded to config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
 export default {
     numberOfConfirmations: 0, // number of block that need to be mined so we assume transaction is mined
     dateFormat: 'YY-MM-DD',
-    ethEndpoint: 'https://ropsten.infura.io/c1GeHOZ7ipPvjO7nDP7l',
-    truffleArtifacts: '../../ESOP/deployed_artifacts/ropsten' // where ABIs and addresses of smart contracts are, used by webpack
+    ethEndpoint: '/api/',
+    // truffleArtifacts: '../../ESOP/deployed_artifacts/ropsten' // where ABIs and addresses of smart contracts are, used by webpack
+    truffleArtifacts: '../../ESOP/build/contracts',
+    ipfs_tags: {
+        "company": "Fifth Force GmbH",
+        "country": "Germany",
+        "hrb-clause": "the commercial register of the local court of Berlin under HRB 179357 B",
+        "repo-url": "git@github.com:Neufund/ESOP.git",
+        "commit-id": "962acdce986897c39677a2293e03dd7e54ee5394",
+        "court-city": "Berlin"
+    }
 }
