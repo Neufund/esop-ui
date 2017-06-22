3483056ca36c86f13a432d21e69a83c6b13d5112
Tomek Banach <tomek@banciur.org>
2017-06-22 22:11:07 +0200

    Changed eth endpoint in config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
     maxNumberOfBlocksToWait: 30, // maximum number of blocks for waiting to transaction to be mined
     dateFormat: 'YY-MM-DD',
     //ethEndpoint: '/api/',
-    ethEndpoint: 'https://neufund.org/nodes/ropsten/',
+    ethEndpoint: 'https://neufund.org/nodes/mainnet/',
     // ethEndpoint: 'https://ropsten.infura.io/',
     truffleArtifacts: '../../ESOP/deployed_artifacts/mainnet', // where ABIs and addresses of smart contracts are, used by webpack
     //truffleArtifacts: '../../ESOP/build/contracts',
