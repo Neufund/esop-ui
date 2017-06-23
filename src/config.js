f7ce54ec23b5b30fde5b9a356ddfc194b618959d
Tomek Banach <tomek@banciur.org>
2017-06-23 10:29:56 +0200

    Added all eth endpoints to config and changed gas price

--- src/config.js
+++ src/config.js
@@ -2,13 +2,17 @@ export default {
     derivationPath: "44'/60'/104'/0",
     //derivationPath: "44'/60'/0'/0",
     gas: 4300000, // close to current mainnet limit
-    gasPriceLimit: 21 * 1000000000, // in gwei you can use http://ethgasstation.info/json/ethgasAPI.json
+    gasPriceLimit: 8 * 1000000000, // in gwei you can use http://ethgasstation.info/json/ethgasAPI.json
     numberOfConfirmations: 2, // number of blocks that need to be mined so we assume transaction is mined
     maxNumberOfBlocksToWait: 30, // maximum number of blocks for waiting to transaction to be mined
     dateFormat: 'YY-MM-DD',
     //ethEndpoint: '/api/',
     ethEndpoint: 'https://neufund.org/nodes/mainnet/',
+    //ethEndpoint: 'https://neufund.net/nodes/mainnet/',
+    //ethEndpoint: 'https://neufund.org/nodes/ropsten/',
+    //ethEndpoint: 'https://neufund.net/nodes/ropsten/',
     //ethEndpoint: 'https://ropsten.infura.io/',
+    //ethEndpoint: 'https://mainnet.infura.io/',
     truffleArtifacts: '../../ESOP/deployed_artifacts/mainnet', // where ABIs and addresses of smart contracts are, used by webpack
     //truffleArtifacts: '../../ESOP/deployed_artifacts/ropsten', // where ABIs and addresses of smart contracts are, used by webpack
     //truffleArtifacts: '../../ESOP/build/contracts',
